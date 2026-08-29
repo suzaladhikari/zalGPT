b3dba6aefa0708aa198caf9f45b280f0243d6388
Sujal Adhikari <adhikarisuzal944@gmail.com>
2026-08-29 14:04:26 -0400

    encoding the whole text using tiktoken

--- main.py
+++ main.py
@@ -8,12 +8,21 @@ torch.manual_seed(455841)
 ### Extracting the key words
 chars = set() 
 with open("../localgpt/data/TinyStories-train.txt", "r", encoding="utf-8") as f:
+    text = f.read()
     for each_line in f:
-        chars.update(f)
+        chars.update(each_line)
 
 chars = sorted(chars)
 
 with open('../localgpt/data/vocab.json', 'w', encoding='utf-8') as f:
     json.dump(chars,f)
 
-print(chars)
+## Setting up the vocab_size 
+vocab_size = len(chars)
+
+## Setting up the encoder and decoder
+enc = tiktoken.get_encoding('gpt2')
+
+## Training and validaiton split 
+data = torch.tensor(enc.encode(text), dtype = torch.long)
+print(data)
