d0fe38c5dc3a747f0e491adcd763256a2058d3b8
Sujal Adhikari <adhikarisuzal944@gmail.com>
2026-08-29 13:33:52 -0400

    loading the dataset and extracting the keywords

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from torch.nn import functional as F
 import tiktoken
 
 torch.manual_seed(455841)
+### Extracting the key words 
+with open("./data/TinyStories-train.txt", "r") as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
