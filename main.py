137e48b50c91656ee79b3d3c1edb51e70f65c005
Sujal Adhikari <adhikarisuzal944@gmail.com>
2026-08-29 13:43:26 -0400

    json dump

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn 
 from torch.nn import functional as F
 import tiktoken
+import json
 
 torch.manual_seed(455841)
 ### Extracting the key words
@@ -11,4 +12,8 @@ with open("../localgpt/data/TinyStories-train.txt", "r", encoding="utf-8") as f:
         chars.update(f)
 
 chars = sorted(chars)
+
+with open('../localgpt/data/vocab.json', 'w', encoding='utf-8') as f:
+    json.dump(chars,f)
+
 print(chars)
