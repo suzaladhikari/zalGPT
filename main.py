055dbc1da750ab1bb4eb083bf4ec7a55d61307e2
Sujal Adhikari <adhikarisuzal944@gmail.com>
2026-09-01 15:12:21 -0400

    started creating batches

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ batch_size = 64
 block_size = 256
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 dropout_layer = 0.2
-eval_iters = 200 ## Each batch loss 
+eval_iters = 500 ## Each batch loss 
 learning_rate = 3e-4
-max_iters = 2000
+max_iters = 3000
 eval_interval = 100
 ### Each time the model gets the data of total 16,384 tokens/step
 
--- rope.py
+++ rope.py
@@ -2,3 +2,26 @@ import torch
 import torch.nn as nn 
 import numpy as np 
 import tiktoken 
+import json 
+
+## Hyperparameters to be used 
+learning_rate = 3e-4
+batch_size = 64
+n_embd = 32 
+total_iterations = 2000
+eval_iter = 20
+
+## Setting up the encoder and decoder 
+enc = tiktoken.get_encoding('gpt2')
+vocab_size = enc.n_vocab ## This gives the total tokens used in the gpt2 encoder
+print(vocab_size)
+## Training and validaiton split
+data = torch.tensor(np.memmap('../localgpt/data/train.bin', dtype = np.uint16, mode = 'r'))
+n = int(0.9 * len(data))
+train_data = data[:n] ## Loading the training data
+validation_data = data[n:] ## Loading the validation data
+
+## Creating the batches 
+def create_batches(split):
+    data = train_data if split == 'train' else validation_data
+    
