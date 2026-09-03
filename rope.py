221de1c12665176351010d901a3a0e502f8897e9
Sujal Adhikari <adhikarisuzal944@gmail.com>
2026-09-03 13:55:24 -0400

    same hyperparameters compared to the baseline

--- rope.py
+++ rope.py
@@ -6,14 +6,14 @@ import tiktoken
 import json 
 
 ## Hyperparameters to be used 
-learning_rate = 3e-4
+n_embd = 32
 batch_size = 64
-n_embd = 32 
-total_iterations = 500
-eval_iter = 20
 block_size = 256
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 dropout_layer = 0.2
-eval_interval = 100
+eval_iters = 500 ## Each batch loss 
+learning_rate = 3e-4
+max_iters = 5000
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 ### For RoPE setup \
