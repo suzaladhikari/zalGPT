aa4b8fbb457d252b2c4cc9efcce04b81be39f976
Peter <adhikarisuzal944@gmail.com>
2026-09-07 08:24:33 -0400

    stored the loss

--- main.py
+++ main.py
@@ -188,6 +188,8 @@ start_time = time.perf_counter()
 for iter in range(max_iters):
     if iter % eval_interval == 0:
         losses = estimate_loss()
+        with open("./loss_tracker/mainlosses.json", 'w') as f:
+            json.dump(losses,f)
         print(f" step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
     xb, yb = get_batch('train')
     xb, yb = xb.to(device), yb.to(device)
