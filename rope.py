ae0f040e95ae71d9fe4c4449f6c0bd95b8cae7c9
Sujal Adhikari <adhikarisuzal944@gmail.com>
2026-09-10 10:25:52 -0400

    added the changed loss in rope.py

--- rope.py
+++ rope.py
@@ -178,8 +178,9 @@ start_time = time.perf_counter()
 for iter in range(max_iters):
     if iter % eval_interval == 0:
         losses = generate_loss()
+        changed_loss = {key:value.item() if torch.is_tensor(value) else value for key,value in loss.items()}
         with open("./loss_tracker/ropeloss.json", "w") as f:
-            json.dump(losses, f)
+            json.dump(changed_loss, f)
         print(f"step{iter}: Test loss {losses['test']}, Train loss {losses['train']}")
 
     ## Setting up for the backwrad propagatoin 
