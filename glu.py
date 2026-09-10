3482b45e199d6dc647e70ea7da55382d0e67d5a6
Sujal Adhikari <adhikarisuzal944@gmail.com>
2026-09-10 09:54:55 -0400

    index issue solved

--- glu.py
+++ glu.py
@@ -37,8 +37,8 @@ print(len(train_data))
 def batch_creater(split):
     data = train_data if split == 'train' else validation_data
     index = torch.randint(len(data)- block_size, (batch_size, ))
-    xb = torch.stack(data[i:i+block_size] for i in range(index))
-    yb = torch.stack(data[i+1:i+block_size+1] for i in range(index))
+    xb = torch.stack([data[i:i+block_size] for i in index])
+    yb = torch.stack([data[i+1:i+block_size+1] for i in index])
     return xb.long(), yb.long()
 
 
