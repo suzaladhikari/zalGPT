0289c0447a4a97bd29c2de6a18a5d3f56ae58f4b
Sujal Adhikari <adhikarisuzal944@gmail.com>
2026-09-11 15:12:44 -0400

    updated the json file location

--- rope.py
+++ rope.py
@@ -184,7 +184,7 @@ for iter in range(max_iters):
         loss = generate_loss()
         loss_history.append({"step":iter, "train":loss['train'].item(), "test": loss['test'].item()})
         print(f"step{iter}: Test loss {loss['test']}, Train loss {loss['train']}")
-        with open('./loss_tracker/glu.json', 'w') as f:
+        with open('./loss_tracker/rope.json', 'w') as f:
             json.dump(loss_history, f, indent=2)
     xb,yb = create_batches('train')
     optimizer.zero_grad(set_to_none=True)
