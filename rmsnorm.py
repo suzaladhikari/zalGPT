0ac571d61cd79f79e2e47fc318ec939b8eef34b9
Sujal Adhikari <adhikarisuzal944@gmail.com>
2026-09-10 20:16:12 -0400

    updated code

--- rmsnorm.py
+++ rmsnorm.py
@@ -8,16 +8,17 @@ import json
 
 ## Setting up the hyperparameters 
 
+
 torch.manual_seed(455841)
-n_embd = 32
+n_embd = 64
 batch_size = 64
 block_size = 256
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 dropout_layer = 0.2
-eval_iters = 250 ## Each batch loss 
+eval_iters = 100 ## Each batch loss 
 learning_rate = 3e-2
 max_iters = 5000
-eval_interval = 500
+eval_interval = 100
 
 ### Encoder and decoder 
 enc = tiktoken.get_encoding('gpt2')
@@ -188,7 +189,7 @@ for iter in range(max_iters):
         loss = generate_loss()
         loss_history.append({"step":iter, "train":loss['train'].item(), "test": loss['test'].item()})
         print(f"step{iter}: Test loss {loss['test']}, Train loss {loss['train']}")
-        with open('./loss_tracker/glu.json', 'w') as f:
+        with open('./loss_tracker/rmsnorm.json', 'w') as f:
             json.dump(loss_history, f, indent=2)
     xb,yb = creating_batches('train')
     optimizer.zero_grad(set_to_none=True)
